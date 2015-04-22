88e56562cb8e98a611583772eed92a734106a850
Weiwei <heymrbox@gmail.com>
2015-04-22 00:27:31 -0700

    still working on detail display

--- CSCI_571_iOS/DetailList.swift
+++ CSCI_571_iOS/DetailList.swift
@@ -10,23 +10,48 @@ import UIKit
 
 class DetailList: UITableViewController {
 
-    @IBOutlet var test_img: UIImageView!
+    @IBOutlet var item_img: UIImageView!
+    
+    @IBOutlet var item_title: UILabel!
+    
+    @IBOutlet var item_price: UILabel!
+    
+    @IBOutlet var item_location: UILabel!
+    
+    @IBOutlet var topRated_img: UIImageView!
+    
+    @IBOutlet var buyNow_btn: UIButton!
+    
+    @IBOutlet var FB_btn: UIButton!
     
     var item_basicInfo = NSDictionary();
     var item_sellerInfo = NSDictionary();
     var item_shippingInfo = NSDictionary();
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
+        
+        displayDetail();
+    }
+    
+    func displayDetail(){
+        
+        /* ---  Display image --- */
         
         if var img_url = NSURL(string: (item_basicInfo["pictureURLSuperSize"] as? String)!){
             if(img_url == ""){
                 img_url = NSURL(string: (item_basicInfo["galleryURL"] as? String)!)!
             }
             if let img_data = NSData(contentsOfURL: img_url){
-                test_img.image = UIImage(data: img_data);
+                item_img.image = UIImage(data: img_data);
             }
         }
+        
+        /* --- Display title --- */
+        if let ttl = item_basicInfo["title"] as? String{
+            item_title.text? = ttl;
+        }
+        
         
     }
     
